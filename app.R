7537d7a33159c1c12fb6aed75febcba1587dd796
Ryan O'Toole <otooleryanjames@gmail.com>
2025-12-07 10:57:02 -0500

    test survey and shiny app

---
+++ app.R
@@ -0,0 +1,14 @@
+library(surveydown)
+
+# Connects to database
+db <- sd_db_connect()
+
+# Main UI
+ui <- sd_ui()
+
+server <- function(input, output, session) {
+  # Main server
+  sd_server(db)
+}
+
+shiny::shinyApp(ui = ui, server = server)
